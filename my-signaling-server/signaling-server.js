c6da32ad8eef622e2b4ef831a16c2f8402d74b34
song57290 <song57290@gmail.com>
2025-03-30 18:28:40 +0900

    test9

--- my-signaling-server/signaling-server.js
+++ my-signaling-server/signaling-server.js
@@ -11,10 +11,10 @@ io.on('connection', socket => {
   console.log(`클라이언트 연결됨: ${socket.id}`);
 
   socket.on('signal', data => {
-    // 연결된 다른 클라이언트에게 신호 데이터 전달 (브로드캐스트)
+    // 같은 룸에 있는 다른 클라이언트에게 신호 데이터 전달
     socket.broadcast.emit('signal', data);
   });
 });
 
 const PORT = process.env.PORT || 3002;
-server.listen(PORT, () => console.log(`시그널링 서버 실행 중: http://localhost:${PORT}`));
+server.listen(PORT, () => console.log(`시그널링 서버 실행 중: 포트 ${PORT}`));
